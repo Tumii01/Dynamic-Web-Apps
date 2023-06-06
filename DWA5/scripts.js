7b3ab1608befcd65f807b68606fce9c6079d01b6
Tumii01 <tumisangnke@gmail.com>
2023-06-06 13:13:23 +0200

    Code Update

--- DWA5/scripts.js
+++ DWA5/scripts.js
@@ -2,29 +2,28 @@ const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-
-  if (!dividend || !divider) {
-    result.innerText = "Division not performed. Both values are required in inputs. Try again";
-    return;
-  }
-
-  if (divider < 0) {
-    result.innerText = "Division not performed. Invalid number provided. Try again";
-    console.error("Invalid number provided");
-    console.trace();
-    return;
-  }
-
-  if (isNaN(dividend) || isNaN(divider)) {
-    result.innerText= "Something critical went wrong. Please reload the page";
-    console.error("Critical error: Non-numeric value provided");
-    console.trace();
-    return;
-  }
-
-
-  result.innerText = Math.floor(dividend / divider);
-});
+    event.preventDefault();
+    const entries = new FormData(event.target);
+    const { dividend, divider } = Object.fromEntries(entries);
+  
+    try {
+      if (!dividend || !divider) {
+        throw new Error("Division not performed. Both values are required in inputs. Try again");
+      }
+  
+      if (isNaN(dividend) || isNaN(divider)) {
+        throw new Error("Something critical went wrong. Please reload the page");
+      }
+  
+      if (divider < 0) {
+        throw new Error("Division not performed. Invalid number provided. Try again");
+      }
+  
+      result.innerText = Math.floor(dividend / divider);
+    } catch (error) {
+      result.innerText = error.message;
+      console.error("Error:", error.message);
+      console.trace();
+    }
+  });
+  
