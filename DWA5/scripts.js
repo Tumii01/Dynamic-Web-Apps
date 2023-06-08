a74cdd5a8925ab2db3fe2c766442177d14247b81
Tumii01 <tumisangnke@gmail.com>
2023-06-08 15:00:04 +0200

    DWA6 Submit.

--- DWA5/scripts.js
+++ DWA5/scripts.js
@@ -5,25 +5,44 @@ form.addEventListener("submit", (event) => {
     event.preventDefault();
     const entries = new FormData(event.target);
     const { dividend, divider } = Object.fromEntries(entries);
-  
-    try {
-      if (!dividend || !divider) {
-        throw new Error("Division not performed. Both values are required in inputs. Try again");
+
+
+
+
+    if (!dividend || !divider) {
+        result.innerText = "Division not performed. Both values are required in inputs. Try again";
+      } else if (divider < 0) {
+        result.innerText = "Division not performed. Invalid number provided. Try again";
+        console.error("Invalid number provided");
+        console.trace();
+      } else if (isNaN(dividend) || isNaN(divider)) {
+        result.innerText = "Something critical went wrong. Please reload the page";
+        console.error("Critical error: Non-numeric value provided");
+        console.trace();
+      } else {
+        result.innerText = Math.floor(dividend / divider);
       }
+    });
+
   
-      if (isNaN(dividend) || isNaN(divider)) {
-        throw new Error("Something critical went wrong. Please reload the page");
-      }
+//     try {
+//       if (!dividend || !divider) {
+//         throw new Error("Division not performed. Both values are required in inputs. Try again");
+//       }
   
-      if (divider < 0) {
-        throw new Error("Division not performed. Invalid number provided. Try again");
-      }
+//       if (isNaN(dividend) || isNaN(divider)) {
+//         throw new Error("Something critical went wrong. Please reload the page");
+//       }
   
-      result.innerText = Math.floor(dividend / divider);
-    } catch (error) {
-      result.innerText = error.message;
-      console.error("Error:", error.message);
-      console.trace();
-    }
-  });
+//       if (divider < 0) {
+//         throw new Error("Division not performed. Invalid number provided. Try again");
+//       }
+  
+//       result.innerText = Math.floor(dividend / divider);
+//     } catch (error) {
+//       result.innerText = error.message;
+//       console.error("Error:", error.message);
+//       console.trace();
+//     }
+//   });
   
