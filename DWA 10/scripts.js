c06c92b3c5a27b0483209a2d4c4d43b5d01db5ea
Tumii01 <tumisangnke@gmail.com>
2023-06-23 14:03:22 +0200

    Code Update.

--- DWA 10/scripts.js
+++ DWA 10/scripts.js
@@ -1,41 +1,50 @@
 
-//Counter Configurations
-
-const MAX_NUMBER = 10
-const MIN_NUMBER = -10
+const MAX_NUMBER = 10;
+const MIN_NUMBER = -10;
+const STEP_AMOUNT = 1;
 
 
 const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
 const add = document.querySelector('[data-key="add"]')
+const reset = document.querySelector('[data-key="reset"]');
 
 const subtractHandler = () => {
-    const newValue = parseInt(number.value) - 1 //Changing string to Number
-    number.value = newValue
+    const newValue = parseInt(number.value) - STEP_AMOUNT;
+   number.value = newValue;
 
-    if (add.disabled === true) {
-        add.disabled = newValue
-    }
+   if (add.disabled === true) {
+    add.disabled = false;
+   }
 
-    if (newValue <= MIN_NUMBER) {
-        subtract.disabled = true
-    }
-}
+   if (newValue <= MIN_NUMBER) {
+    subtract.disabled = true;
+   }
+};
 
 const addHandler = () => {
-    const newValue = parseInt(number.value) + 1 //Changing string to Number
-    number.value = newValue
+    const newValue = parseInt(number.value) + STEP_AMOUNT;
+    number.value = newValue;
 
     if (subtract.disabled === true) {
-        subtract.disabled = false
+        subtract.disabled = false;
     }
-    if (newValue >= MAX_NUMBER) {
-        add.disabled = true
-    }
-}
 
-//Clicking
+    if(newValue >=MAX_NUMBER) {
+        add.disabled = true;
+        }
+       
+};
+//added a handleReset button which is functonal and is able to reset tally count to 0
+const handleReset = () => {
+    number.value = 0;
+    subtract.disabled = true;
+    add.disabled = false;
+    //added alert when reset button has been clicked
+    window.alert("Counter has been reset")
+};
 
-subtract.addEventListener('click', subtractHandler)
 
-add.addEventListener('click', addHandler)
+subtract.addEventListener('click', subtractHandler);
+add.addEventListener('click',  addHandler);
+reset.addEventListener('click', handleReset);
